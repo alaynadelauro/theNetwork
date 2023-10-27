c0043e28c9afc6f3e200ee32b1680b4f338cd266
alaynadelauro <andelauro@yahoo.com>
2023-10-27 10:48:31 -0600

    init

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <h1>Sup</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+
+  .home-card {
+    width: 50vw;
+
+    >img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+}
+</style>
