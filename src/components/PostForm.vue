719b5fcb847c940c638c52927574bc03b05e83d5
alaynadelauro <andelauro@yahoo.com>
2023-10-27 13:17:34 -0600

    modal has its own component, works

---
+++ src/components/PostForm.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="modal fade modal-xl" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">What would you like to say?</h1>
+                    <button type="button" class="btn-close ms-0" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <!-- SECTION Form starts here -->
+                    <form class="p-2 pb-3">
+                        <div>
+                            <label for="image" class="form-label">Image</label>
+                            <input maxlength="500" type="url" class="form-control" placeholder="Would you like to include an image?">
+                        </div>
+                        <div>
+                            <label for="body" class="form-label">Body</label>
+                            <textarea rows="5" required maxlength="5000" class="form-control" placeholder="Please include a body in your post"></textarea>
+                        </div>
+                        <div class="d-flex text-end mt-4 mb-0 me-3 justify-content-end">
+                            <div>
+                                <button type="submit" class="btn submitButton">Submit</button>
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            </div>
+                        </div>
+
+                    </form>
+                    <!-- SECTION form ends here -->
+                </div>
+                <div class="modal-footer">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { computed, reactive, onMounted } from 'vue';
+export default {
+    setup() {
+        return {}
+    }
+};
+</script>
+
+
+<style lang="scss" scoped></style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,57 +2,21 @@
   <div class="container-fluid">
     <div class="row justify-content-start">
       <div class="col-2 h-100">
-        <h1>Prof</h1>
+        <h1>Profile</h1>
         <!-- <button @click="makePost()" class="btn"><i class="mdi mdi-plus"></i></button> -->
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
           <i class="mdi mdi-plus"></i>
         </button>
-        <!-- Modal -->
-        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5" id="staticBackdropLabel">What would you like to say?</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                <!-- SECTION Form starts here -->
-                <form @submit.prevent="createBlog()" class="p-4 pb-3">
-                  <div class="d-flex">
-                    <div class="d-block w-25">
-                      <div class="">
-                        <label for="title" class="form-label">Title</label>
-                        <input type="text" required class="form-control" placeholder="Title..">
-                      </div>
-                      <div class="mt-3">
-                        <label for="imgUrl" class="form-label">Blog Image</label>
-                        <input maxlength="500" type="url" required class="form-control" placeholder="Article picture..">
-                      </div>
-                    </div>
-                    <div class="mx-3 w-75">
-                      <label for="body" class="form-label">Body</label>
-                      <textarea rows="5" required class="form-control" placeholder="Blog content..."></textarea>
-                    </div>
-                  </div>
-                  <div class="text-end mt-4 mb-0 me-3">
-                    <button type="submit" class="btn submitButton">Submit</button>
-                  </div>
-                </form>
-                <!-- SECTION form ends here -->
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-              </div>
-            </div>
-          </div>
+        <div>
+          <PostForm />
         </div>
+        <!-- SECTION modal goes here -->
       </div>
       <div class="col-8 text-center">
         <h1>This is the Home Page</h1>
         <div class="row justify-content-center mt-5">
           <div v-for="post in posts" :key="post.id" class="col-9 d-flex postCard m-2 px-0">
             <PostCardComponent :postProp="post" />
-
           </div>
         </div>
       </div>
@@ -66,11 +30,12 @@
 
 <script>
 import { logger } from '../utils/Logger';
-import Pop from '../utils/Pop.js';
+// import Pop from '../utils/Pop.js';
 import { postService } from '../services/PostService.js'
 import { computed, onMounted } from 'vue';
 import { AppState } from '../AppState.js'
 import PostCardComponent from '../components/PostCardComponent.vue';
+import PostForm from '../components/PostForm.vue'
 
 export default {
   setup() {
@@ -101,7 +66,7 @@ export default {
       }
     };
   },
-  components: { PostCardComponent }
+  components: { PostCardComponent, PostForm }
 }
 </script>
 
